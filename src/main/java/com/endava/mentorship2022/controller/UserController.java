11ed4e4d5b5783f1d159fe3ddff329151ae00ee9
alistanciu <alin-angel-madalin.stanciu@endava.com>
2022-06-15 10:24:20 +0300

    replace returned type in controller

--- src/main/java/com/endava/mentorship2022/controller/UserController.java
+++ src/main/java/com/endava/mentorship2022/controller/UserController.java
@@ -3,7 +3,6 @@ package com.endava.mentorship2022.controller;
 import com.endava.mentorship2022.model.User;
 import com.endava.mentorship2022.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,15 +18,13 @@ public class UserController {
     UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        List<User> users = userService.findAll();
-        return ResponseEntity.ok(users);
+    public List<User> findAll() {
+        return userService.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id) {
-        User user = userService.findById(id);
-        return ResponseEntity.ok(user);
+    public User findById(@PathVariable Long id) {
+        return userService.findById(id);
     }
 
 }
