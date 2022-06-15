204fb5a42ab16e69fce3781d35daeaa44b01a9f0
Danut Florentin Radu <Danut-Florentin.Radu@endava.com>
2022-06-15 16:22:36 +0300

    Added CartItem Entity

---
+++ src/main/java/com/endava/mentorship2022/model/CartItem.java
@@ -0,0 +1,31 @@
+package com.endava.mentorship2022.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Transient
+    public float getSubtotal() {
+        return product.getPrice() * quantity;
+    }
+}
