7c367e0f54801d4560b3a2f7d4fceb587756b567
alistanciu <alin-angel-madalin.stanciu@endava.com>
2022-06-15 16:25:22 +0300

    added columns birthDate and status in user entity + length for each field

--- src/main/java/com/endava/mentorship2022/model/User.java
+++ src/main/java/com/endava/mentorship2022/model/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @NoArgsConstructor
 @Data
@@ -14,26 +15,32 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
+    @Column(nullable = false, length = 40)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 40)
     private String firstName;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 40)
     private String lastName;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, length = 100, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 64)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 200)
     private String address;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 15)
     private String phoneNumber;
+
+    @Column(nullable = false)
+    private Date birthDate;
+
+    @Column(nullable = false, length = 10, columnDefinition = "pending")
+    private String status;
 
 }
