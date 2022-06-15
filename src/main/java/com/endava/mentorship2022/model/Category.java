ae960edffb05777cbbc83c9c09a9b2c1f34eb201
Danut Florentin Radu <Danut-Florentin.Radu@endava.com>
2022-06-15 12:30:03 +0300

    Added Category Entity

--- src/main/java/com/endava/mentorship2022/model/Category.java
+++ src/main/java/com/endava/mentorship2022/model/Category.java
@@ -1,4 +1,28 @@
 package com.endava.mentorship2022.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String alias;
+
+    @OneToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
 }
