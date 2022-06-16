e47f5200a9739c8a93ac0a8e7c9d74ac9ef59258
mmandoiu <Madalina.Mandoiu@endava.com>
2022-06-16 16:19:39 +0300

    long type for id field instead of Long

--- src/main/java/com/endava/mentorship2022/model/TechnicalDetails.java
+++ src/main/java/com/endava/mentorship2022/model/TechnicalDetails.java
@@ -11,11 +11,14 @@ public class TechnicalDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
-    private Long id;
+    private long id;
+
     @Column(name="name",nullable = false, length=250)
     private String name;
+
     @Column(name="value",nullable = false, length=250)
     private String value;
+
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
