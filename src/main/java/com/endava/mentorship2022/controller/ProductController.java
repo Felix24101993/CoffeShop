c5f0a677d0305ecad9eb53d551660e7a740777be
Felix Marinau <marinaufelixdumitru@gmail.comgit config --global --listgit config --global user.email marinaufelixdumitru@gmail.com>
2022-06-17 12:26:53 +0300

    Adding Product Controller

---
+++ src/main/java/com/endava/mentorship2022/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.endava.mentorship2022.controller;
+
+
+import com.endava.mentorship2022.model.Product;
+import com.endava.mentorship2022.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+    @GetMapping
+    public List<Product> findAll(){
+        return productService.listAllProducts();
+    }
+    @GetMapping("{id}")
+    public Product findById(@PathVariable long id){
+        return productService.findById(id);
+    }
+}
