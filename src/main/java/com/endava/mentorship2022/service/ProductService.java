c536c7fdc5527f57c0ffd1aa9bb41fe81486568b
Felix Marinau <marinaufelixdumitru@gmail.comgit config --global --listgit config --global user.email marinaufelixdumitru@gmail.com>
2022-06-17 12:26:36 +0300

    Adding Product Service and ProductNotFound

---
+++ src/main/java/com/endava/mentorship2022/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.endava.mentorship2022.service;
+
+import com.endava.mentorship2022.exception.ProductNotFound;
+import com.endava.mentorship2022.model.Product;
+import com.endava.mentorship2022.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> listAllProducts() {
+       return productRepository.findAll();
+}
+
+    public Product findById(long id) {
+        return productRepository.findById(id).orElseThrow(()-> new ProductNotFound("Product: " + id + " not found!"));
+    }
+}
