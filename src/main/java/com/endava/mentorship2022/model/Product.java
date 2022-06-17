40122ea24919cdeab2c4d1170ee73de23d46a9a5
Felix Marinau <marinaufelixdumitru@gmail.comgit config --global --listgit config --global user.email marinaufelixdumitru@gmail.com>
2022-06-17 12:25:31 +0300

    Adding Product entity

--- src/main/java/com/endava/mentorship2022/model/Product.java
+++ src/main/java/com/endava/mentorship2022/model/Product.java
@@ -1,4 +1,34 @@
 package com.endava.mentorship2022.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "name", length = 200, nullable = false)
+    private String name;
+    @Column(name = "alias", length = 200, nullable = false)
+    private String alias;
+    @Column(name = "description", length = 3000, nullable = false)
+    private String description;
+    @Column(name = "brand", length = 80, nullable = false)
+    private String brand;
+    @Column(name = "price", nullable = false)
+    private float price;
+    @Column(name = "stock", nullable = false)
+    private int stock;
+    @Column(name = "enabled", nullable = false)
+    private boolean enabled;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
