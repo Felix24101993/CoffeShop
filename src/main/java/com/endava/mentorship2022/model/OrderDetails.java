0659601cd36724bc38675580a0a226421236078f
Emilian Mihai Covreag <Emilian-Mihai.Covreag@endava.com>
2022-06-20 15:45:03 +0300

    Add OrderDetails Entity

--- src/main/java/com/endava/mentorship2022/model/OrderDetails.java
+++ src/main/java/com/endava/mentorship2022/model/OrderDetails.java
@@ -1,4 +1,39 @@
 package com.endava.mentorship2022.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Formula;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "order_details")
 public class OrderDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "quantity", nullable = false)
+    private short quantity;
+
+    @Column(name = "unit_price", nullable = false)
+    private float unitPrice;
+
+    @Column(name = "subtotal")
+    @Formula("unit_price * quantity")
+    private float subtotal;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    // TODO
+//    @ManyToOne
+//    @JoinColumn(name = "product_id")
+//    private Product product;
+
 }
