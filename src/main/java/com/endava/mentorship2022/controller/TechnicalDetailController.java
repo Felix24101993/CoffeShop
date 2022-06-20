640c4f7748789baf672d3b52e22c1ed17bcc6f5e
mmandoiu <Madalina.Mandoiu@endava.com>
2022-06-20 10:10:51 +0300

    updated controller => used plural for @RequestMapping

--- src/main/java/com/endava/mentorship2022/controller/TechnicalDetailController.java
+++ src/main/java/com/endava/mentorship2022/controller/TechnicalDetailController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/technicaldetail")
+@RequestMapping("/technicaldetails")
 @AllArgsConstructor
 public class TechnicalDetailController {
     private final TechnicalDetailService technicalDetailService;
