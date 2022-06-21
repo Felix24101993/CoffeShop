9f6fba8da3a138b087f32f3db03d39145d47ecc2
Felix Marinau <marinaufelixdumitru@gmail.comgit config --global --listgit config --global user.email marinaufelixdumitru@gmail.com>
2022-06-21 09:55:39 +0300

    Modifying Product Model

--- src/main/java/com/endava/mentorship2022/model/Product.java
+++ src/main/java/com/endava/mentorship2022/model/Product.java
@@ -13,28 +13,26 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @Column(name = "name", length = 200, nullable = false)
-
     private String name;
+
     @Column(name = "alias", length = 200, nullable = false, unique = true)
-
     private String alias;
+
     @Column(name = "description", length = 3000, nullable = false, unique = true)
-
     private String description;
+
     @Column(name = "brand", length = 80, nullable = false)
-
     private String brand;
+
     @Column(name = "price", nullable = false)
-
     private float price;
-    @Column(name = "stock", nullable = false)
 
+    @Column(name = "stock", nullable = false)
     private int stock;
     @Column(name = "enabled", nullable = false)
-
     private boolean enabled;
-
 
     @ManyToOne
     @JoinColumn(name = "category_id")
