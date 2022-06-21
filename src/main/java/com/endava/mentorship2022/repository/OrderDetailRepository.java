41ede7438b7c79ee8337d2463ef887fa1639f6bb
Emilian Mihai Covreag <Emilian-Mihai.Covreag@endava.com>
2022-06-21 09:50:49 +0300

    Rename OrderDetailsRepository to OrderDetailRepository

--- src/main/java/com/endava/mentorship2022/repository/OrderDetailRepository.java
+++ src/main/java/com/endava/mentorship2022/repository/OrderDetailRepository.java
@@ -1,13 +1,13 @@
 package com.endava.mentorship2022.repository;
 
-import com.endava.mentorship2022.model.OrderDetails;
+import com.endava.mentorship2022.model.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> {
-    List<OrderDetails> findByOrderId(long orderId);
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+    List<OrderDetail> findByOrderId(long orderId);
 
 }
