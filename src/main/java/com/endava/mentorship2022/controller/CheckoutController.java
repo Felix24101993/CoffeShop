2114e0ddce65fb06536c206240f151296ba9c406
Danut Florentin Radu <Danut-Florentin.Radu@endava.com>
2022-06-23 14:38:09 +0300

    Added Checkout Controller & Service.

---
+++ src/main/java/com/endava/mentorship2022/controller/CheckoutController.java
@@ -0,0 +1,21 @@
+package com.endava.mentorship2022.controller;
+
+import com.endava.mentorship2022.service.CheckoutService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/checkout")
+public class CheckoutController {
+
+    private final CheckoutService checkoutService;
+
+    @PostMapping("/{userId}")
+    public String placeOrder(@PathVariable long userId) {
+        return checkoutService.placeOrder(userId);
+    }
+}
