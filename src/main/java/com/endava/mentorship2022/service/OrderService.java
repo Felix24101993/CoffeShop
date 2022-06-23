f1d7caed3a5889de5466d818efbc1a56a30e22fd
mirionescu <miruna.balanescu@endava.com>
2022-06-23 14:33:15 +0300

    implement createOrder(long userId, List<CartItem> cartItems) and calculateTotal()

--- src/main/java/com/endava/mentorship2022/service/OrderService.java
+++ src/main/java/com/endava/mentorship2022/service/OrderService.java
@@ -2,15 +2,14 @@
 package com.endava.mentorship2022.service;
 
 import com.endava.mentorship2022.exception.OrderNotFound;
-import com.endava.mentorship2022.model.Order;
-import com.endava.mentorship2022.model.OrderStatus;
-import com.endava.mentorship2022.model.User;
+import com.endava.mentorship2022.model.*;
 import com.endava.mentorship2022.repository.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -20,7 +19,7 @@ public class OrderService {
 
     private final UserService userService;
 
-    public List<Order> findAll(){
+    public List<Order> findAll() {
         return orderRepository.findAll();
     }
 
@@ -47,6 +46,40 @@ public class OrderService {
         Order order = findById(id);
         order.setStatus(newStatus);
         return orderRepository.save(order);
+    }
+
+    public Order createOrder(long userId, List<CartItem> cartItems) {
+
+        User user = userService.findById(userId);
+        Order newOrder = new Order();
+        newOrder.setTotal(calculateTotal(cartItems));
+        newOrder.setUser(user);
+        Set<OrderDetail> orderDetails = newOrder.getOrderDetails();
+
+        for (CartItem cartItem : cartItems) {
+            Product product = cartItem.getProduct();
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.setOrder(newOrder);
+            orderDetail.setProduct(product);
+            orderDetail.setQuantity(cartItem.getQuantity());
+            orderDetail.setUnitPrice(product.getPrice());
+            orderDetail.setSubtotal(cartItem.getSubtotal());
+            orderDetails.add(orderDetail);
+        }
+
+        return orderRepository.save(newOrder);
+
+    }
+
+    private float calculateTotal(List<CartItem> cartItems) {
+
+        float total = 0.0F;
+        for (CartItem cartItem : cartItems) {
+            total += cartItem.getSubtotal();
+        }
+
+        return total;
+
     }
 
 }
