401753bfdcc08dcd18aa7d2ca9c69b82f9cedb68
Angel Stanciu <102602542+angelstanciu@users.noreply.github.com>
2022-06-28 15:50:38 +0300

    added Role class to match roles table in DB

---
+++ src/main/java/com/endava/mentorship2022/model/Role.java
@@ -0,0 +1,26 @@
+package com.endava.mentorship2022.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(nullable = false, length = 15)
+    private String name;
+
+}
