6ba8c15577d800e98afea47062a0241099024a8d
Emilian Mihai Covreag <Emilian-Mihai.Covreag@endava.com>
2022-07-04 17:19:27 +0300

    Implement Test Method for saving many products

--- src/test/java/com/endava/mentorship2022/service/ProductServiceTest.java
+++ src/test/java/com/endava/mentorship2022/service/ProductServiceTest.java
@@ -1,10 +1,8 @@
 package com.endava.mentorship2022.service;
 
 import com.endava.mentorship2022.exception.ProductNotFound;
-import com.endava.mentorship2022.exception.UserNotFound;
-import com.endava.mentorship2022.model.*;
+import com.endava.mentorship2022.model.Product;
 import com.endava.mentorship2022.repository.ProductRepository;
-import com.endava.mentorship2022.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,23 +10,28 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+import java.util.stream.Collectors;
 
-import static com.endava.mentorship2022.model.UserStatus.ACTIVE;
-import static com.endava.mentorship2022.model.UserStatus.PENDING;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
 
     @Mock
     private ProductRepository productRepository;
+
+    @Mock
+    private CategoryService categoryService;
 
     @InjectMocks
     private ProductService productService;
@@ -178,6 +181,66 @@ class ProductServiceTest {
 
         // then
         assertThat(newProduct).isEqualTo(product);
+    }
+
+    @Test
+    void shouldSaveLotsOfProducts() throws IOException {
+        // given
+        List<Product> productsToBeSaved = createListOfProducts();
+
+        for (Product product : productsToBeSaved) {
+            when(productRepository.save(product)).thenReturn(product);
+        }
+
+        // when
+        List<Product> productsSaved = new ArrayList<>();
+        for (Product product : productsToBeSaved) {
+            Product productSaved = productService.saveProduct(product);
+            productsSaved.add(productSaved);
+        }
+
+        // then
+        assertThat(productsSaved).hasSize(100);
+
+        for (int i = 0; i < productsSaved.size(); i++) {
+            assertThat(productsSaved.get(i)).isEqualTo(productsToBeSaved.get(i));
+        }
+
+    }
+
+    // method for creating a list of products using the words from a text file
+    private List<Product> createListOfProducts() throws IOException {
+
+        final int MAX_PRODUCTS = 100;                                   // how many products you want to save
+
+        List<Product> productList = new ArrayList<>();
+
+        List<String> wordList = Files
+                .lines(Paths.get("src/main/greatexpectations.txt"))  // get the lines from a text file as a stream
+                .map(line -> line.split("[^A-Za-z]"))              // create a stream of String[] only with letters
+                .map(Arrays::asList)                                     // create a stream of List<String> - easy to work with
+                .flatMap(list -> list.stream())                          // create a stream of words
+                .filter(word -> word.length() > 3 && word.length() < 20) // keep the words with length 3-20
+                .limit(MAX_PRODUCTS * 5)                          // limit the list size
+                .collect(Collectors.toList());                            // save the stream into a list
+
+        // loop through the lists, create and add a new Product to the list on each iteration
+        for (int i = 0; i < wordList.size() - 4; i += 5) {
+            Product product = new Product();
+            //selects 3 words and sets the name with blanks between words
+            product.setName(wordList.get(i) + " " + wordList.get(i + 1) + " " + wordList.get(i + 2));
+            //selects 3 words and sets the alias with hyphens between words
+            product.setAlias(wordList.get(i) + "-" + wordList.get(i + 1) + "-" + wordList.get(i + 2));
+            product.setDescription(wordList.get(i + 3));
+            product.setBrand(wordList.get(i + 4));
+            product.setPrice((float) (Math.random() * 1000) + 1);
+            product.setStock((int) (Math.random() * 100) + 1);
+            product.setEnabled(true);
+            product.setCategory(null);
+            productList.add(product);
+        }
+
+        return productList;
     }
 
 }
